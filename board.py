513f6f396d2d3bce2ea3daebb1797e5c176c2e8c
rayanf <rayanforsat@gmail.com>
2023-01-14 20:26:54 +0330

    Done

--- board.py
+++ board.py
@@ -3,29 +3,277 @@ import sys
 import random
 import networkx as nx
 import numpy as np
+from boardCoordinate import *
+import json
 
 
 class Board:
-    def __init__(self):
+    def __init__(self,walk_punishment,flag_reward,target_reward,back_punishment,been_punishment,alpha,gamma):
+        self.walk_punishment = walk_punishment
+        self.flag_reward = flag_reward
+        self.target_reward = target_reward
+        self.back_punishment = back_punishment
+        self.been_punishment = been_punishment
+        self.alpha = alpha
+        self.gamma = gamma
+    
+        pygame.init()
         self.w = 600
         self.h = 600
-        self.squares_hw = 60
-        
+
+        self.frameIter = 0
         self.fps = 60
         self.screen = pygame.display.set_mode((self.w, self.h))
         self.clock = pygame.time.Clock()
 
-        self.player = Player([290,290])
+        self.player = Player([30,30])
+        self.playerGp = pygame.sprite.Group()
+        self.playerGp.add(self.player)
+
         self.flags = []
-        self.create_flags()
         self.flagsObject = pygame.sprite.Group()
 
+        self.blocks = []
+        self.blocksObject = pygame.sprite.Group()
 
-    def create_flags(self):
-        flagObj = Flag([30,30])
+        self.target = Target([570,570])
+        self.targetObject = pygame.sprite.Group()
+        self.targetObject.add(self.target)
+
+        self.v_values = np.zeros((10,10))
+        self.lastState = None
+        
+        self.nodes = [[Node(i,j) for i in range(10)] for j in range(10)]
+        self.create_nodes()
+        
+        self.currentState = (0,0)
+        self.seenNodes = []
+
+    def resetQtable(self):
+        self.Qtable = np.zeros((10,10,4))
+        arr_reshaped = self.Qtable.reshape(self.Qtable.shape[0], -1)
+        np.savetxt('Qtable.txt', arr_reshaped)
+
+    def saveQtable(self):
+        pygame.image.save(self.screen, "screenshot.jpg")
+        self.Qtable = [[self.nodes[i][j].q for j in range(10)] for i in range(10)]
+        self.Qtable = np.array(self.Qtable)
+        arr_reshaped = self.Qtable.reshape(self.Qtable.shape[0], -1)
+        np.savetxt('Qtable.txt', arr_reshaped)
+
+    def loadQtable(self):
+        self.Qtable = np.loadtxt('Qtable.txt')
+        self.Qtable = self.Qtable.reshape(10,10,4)
+        for i in range(10):
+            for j in range(10):
+                self.nodes[i][j].q = self.Qtable[(i,j)]
+
+
+    def run(self,direction,step,iters):
+        self.seenNodes.append(self.currentState)
+        self.lastlastState = self.lastState
+        self.lastState = ((self.player.rect.center[1]-30) // 60, (self.player.rect.center[0]-30) // 60)
+        self.player.move(direction)
+        self.currentState = ((self.player.rect.center[1]-30) // 60, (self.player.rect.center[0]-30) // 60)
+        flag,target = self.update_qValues(direction)
+
+        if flag:
+            self.player.score += 1
+            self.remove_flag()
+        
+
+        self.update_screen(step,iters)
+        return flag,target
+
+    def update_qValues(self,direction):
+        r,flag,target = self.getActionReward()
+        currentQ = self.nodes[self.lastState[0]][self.lastState[1]].q[self.getActionIndex(direction)]
+        maxQ = max(self.nodes[self.currentState[0]][self.currentState[1]].q)
+        newQ = (1 - self.alpha) * currentQ + self.alpha * (r + self.gamma * maxQ)
+        self.nodes[self.lastState[0]][self.lastState[1]].q[self.getActionIndex(direction)] = newQ
+        return flag,target
+
+    def getActionIndex(self,direction):
+        if direction == 'left':
+            return 0
+        elif direction == 'up':
+            return 1
+        elif direction == 'right':
+            return 2
+        elif direction == 'down':
+            return 3
+
+    def getActionReward(self):
+        flag = False
+        target = False
+        reward = 0
+        reward += self.walk_punishment
+        if self.rich_flag():
+            reward += self.flag_reward
+            flag = True
+        if self.rich_target():
+            reward += self.target_reward
+            target = True
+        if self.back_move():
+            reward += self.back_punishment 
+        if self.been_move():
+            reward += self.been_punishment
+
+        return reward,flag,target
+            
+    def been_move(self):
+        if self.currentState in self.seenNodes:
+            return True
+        else: return False
+
+    def remove_flag(self):  
+        for i in range(len(self.flags)):
+            if self.flags[i].rect.center == self.player.rect.center:
+                ii = (self.flags[i].rect.center[0] - 30) // 60
+                jj = (self.flags[i].rect.center[1] - 30) // 60
+                self.flagsObject.remove(self.flags[i])
+                self.flags.pop(i)
+                self.nodes[jj][ii].type = "way"
+                break
+
+    def rich_flag(self):
+        if  pygame.sprite.spritecollide(self.player, self.flagsObject, True):
+            return True
+        else: return False
+
+    def rich_target(self):
+        if  pygame.sprite.spritecollide(self.player, self.targetObject, False):
+            return True
+        else: return False
+
+    def back_move(self):
+        if self.lastlastState == self.currentState:
+            return True
+        else: return False
+
+    def create_nodes(self):
+        for i in range(10):
+            for j in range(10):
+                if i == 0:
+                    self.nodes[i][j].q[1] = np.NINF
+                elif i == 9:
+                    self.nodes[i][j].q[3] = np.NINF
+                if j == 0:
+                    self.nodes[i][j].q[0] = np.NINF
+                elif j == 9:
+                    self.nodes[i][j].q[2] = np.NINF
+                if types[i][j] == 0:
+                    self.nodes[i][j].type = "start"
+                    
+                elif types[i][j] == 1:
+                    self.nodes[i][j].type = "way"
+                    
+                elif types[i][j] == 2:
+                    self.nodes[i][j].type = "flag"
+                    self.create_flag(ItoC(j+1,i+1))
+                elif types[i][j] == 3:
+                    self.nodes[i][j].type = "block"
+                    self.create_block(ItoC(j+1,i+1))
+                elif types[i][j] == 4:
+                    self.nodes[i][j].type = "target"
+                    
+    def create_flag(self,pos):
+        flagObj = Flag(pos)
         self.flags.append(flagObj)
         self.flagsObject.add(flagObj)
 
+    def create_block(self,pos):
+        blockObj = Block(pos)
+        self.blocks.append(blockObj)
+        self.blocksObject.add(blockObj)
+
+    def reset(self,iters):
+        self.seenNodes = []
+        self.lastState = None
+        self.curentState = (0,0)
+
+        self.player.rect.center = [30,30]
+        self.flags = []
+        self.flagsObject = pygame.sprite.Group()
+        self.blocks = []
+        self.blocksObject = pygame.sprite.Group()
+        self.target = Target([570,570])
+        self.targetObject = pygame.sprite.Group()
+        self.targetObject.add(self.target)
+
+        self.create_nodes()
+        self.screen = pygame.display.set_mode([self.w, self.h])
+        self.frameIter = 0
+        self.player.score = 0
+        self.update_screen(0,iters)
+
+    def update_screen(self,step,iters):
+        self.screen = pygame.display.set_mode([self.w, self.h])
+        self.screen.fill((112, 146, 190))
+        self.playerGp.draw(self.screen)  
+        self.flagsObject.draw(self.screen) 
+        self.blocksObject.draw(self.screen)
+        self.targetObject.draw(self.screen)
+
+        score_font = pygame.font.SysFont("comicsansms", 10) 
+        itter_font = pygame.font.SysFont("comicsansms", 10) 
+
+        value = score_font.render("Step: " + str(step), True, (255, 255, 255))
+        self.screen.blit(value, [60, 0])
+
+        value = itter_font.render("Iters: " + str(iters), True, (255, 255, 255))
+        self.screen.blit(value, [60, 15])
+
+        # self.write_v()
+        self.write_q()
+        self.draw_borders()
+        pygame.display.update()  
+        pygame.display.flip()   
+
+    def draw_borders(self):
+        for i in range(9):
+            pygame.draw.line(self.screen, (0,0,0), ((i+1)*60,0),((i+1)*60,600) , 1)
+            pygame.draw.line(self.screen, (0,0,0), (0,(i+1)*60),(600,(i+1)*60) , 1)
+
+
+    def write_q(self):
+        for i in range(10):
+            for j in range(10):
+                node = self.nodes[i][j]
+                direction =np.argmax(node.q)
+                if node.type == "block":   
+                    continue
+
+                if direction == 0:
+                    leftQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[0]), True, (255, 255, 255))
+                else:
+                    leftQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[0]), True, (0, 0, 0))
+                self.screen.blit(leftQ, [node.x-27,node.y-10])
+
+                if direction == 1:
+                    upQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[1]), True, (255, 255, 255))
+                else: 
+                    upQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[1]), True, (0, 0, 0))
+                self.screen.blit(upQ, [node.x-5,node.y-30])
+
+                if direction == 2:
+                    rightQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[2]), True, (255, 255, 255))
+                else:
+                    rightQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[2]), True, (0, 0, 0))
+                self.screen.blit(rightQ, [node.x+5,node.y-4])
+
+                if direction == 3:                
+                    downQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[3]), True, (255, 255, 255))
+                else:
+                    downQ = pygame.font.SysFont("comicsansms", 10).render("{:.2f}".format(node.q[3]), True, (0, 0, 0))
+                self.screen.blit(downQ, [node.x-5,node.y+15])
+
+
+    def write_v(self):
+        for i in range(10):
+            for j in range(10):
+                v_value = pygame.font.SysFont("comicsansms", 15).render(str(self.v_values[i,j]), True, (0, 0, 0))
+                self.screen.blit(v_value, [j*60+15, i*60+15])
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,position):
@@ -37,23 +285,62 @@ class Player(pygame.sprite.Sprite):
         self.vx = 20
         self.vy = 20
         self.rect.center = position
-        self.direction = pygame.K_RIGHT
-
-    def run(self):
-        if self.direction != None:
-            for i in range(2):  
-                if self.direction == self.move[i]:  
-                    self.rect.x += self.vx * [-1, 1][i]  
-
-                elif self.direction == self.move[2:4][i]:  
-                    self.rect.y += self.vy * [-1, 1][i]
+        self.score = 0
     
+    def move(self,direction):
+        if direction == "left":
+            self.rect.x -= 60
+        elif direction == "right":
+            self.rect.x += 60
+        elif direction == "up":
+            self.rect.y -= 60
+        elif direction == "down":
+            self.rect.y += 60
 
 class Flag(pygame.sprite.Sprite):
     def __init__(self,position):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([60,60])
         self.rect = self.image.get_rect()
-        self.image.fill((128,255,128))
+        self.image.fill((29,232,147))
         self.rect.center = position
+
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([60,60])
+        self.rect = self.image.get_rect()
+        self.image.fill((64, 61, 56))
+        self.rect.center = position
+
+class Target(pygame.sprite.Sprite):
+    def __init__(self,position):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([60,60])
+        self.rect = self.image.get_rect()
+        self.image.fill((28,145,62))
+        self.rect.center = position
+
+
+def ItoC(i,j):
+    return (i*60-30,j*60-30)
+
+def CtoI(x,y):
+    return ((x-30)//60,(y-30)//60)
+
+class Node:
+    def __init__(self,i,j):
+        self.x = i*60+30
+        self.y = j*60+30
+        
+        self.i = i
+        self.j = j
+
+        self.neighbors = [None,None,None,None]
+        self.q = [0,0,0,0] # L U R D 
+
+        self.V = 0
+
+        self.type = None
 
---
+++ main.py
@@ -0,0 +1,124 @@
+from board import Board
+import random
+import numpy as np
+
+walk_punishment = -0.5
+flag_reward = 50
+target_reward = 200
+back_punishment = -40
+been_punishment = -10
+alpha = 0.1
+gamma = 0.9 
+
+step_treshhold = 20
+iters = 200
+
+def play(iters):
+    game = Board(walk_punishment,flag_reward,target_reward,back_punishment,been_punishment,alpha,gamma)
+    game.reset(iters)
+    
+    # game.resetQtable()
+    
+    game.loadQtable()
+    agent = Agent(game)
+
+    
+    step = 0
+    while True:
+        step += 1 
+
+        state = agent.get_state()
+        choices = agent.get_choices(state)        
+        action = agent.make_action(state,choices,iters)
+        flag,target = game.run(action,step,iters)
+        
+        if flag :
+            step -= 15
+            
+        if target:
+            step = -np.NINF
+        if step > step_treshhold:
+            iters += 1
+            # game.saveQtable()
+            game.reset(iters)
+            step = 0
+
+    
+
+
+
+class Agent:
+    def __init__(self,game):
+        self.game = game
+        self.qTable = None
+        self.choices = ['left','up','right','down']
+
+    def get_state(self):
+        cords = self.game.player.rect.center
+        i = (cords[0] - 30) // 60
+        j = (cords[1] - 30) // 60
+        return self.game.nodes[j][i]
+    
+    def get_choices(self,state):
+        i = state.j
+        j = state.i
+        choices = ['left','up','right','down']
+        if j == 0:
+            choices.remove('left')
+        elif j == 9:
+            choices.remove('right')
+        if i == 0:
+            choices.remove('up')
+        elif i == 9:
+            choices.remove('down')
+
+        if i > 0 and self.game.nodes[i-1][j].type == 'block':
+            try:
+                choices.remove('up')
+            except:
+                pass
+        if i < 9 and self.game.nodes[i+1][j].type == 'block':
+            try:
+                choices.remove('down')
+            except:
+                pass
+        if j > 0 and self.game.nodes[i][j-1].type == 'block':
+            try:
+                choices.remove('left')
+            except:
+                pass
+        if j < 9 and self.game.nodes[i][j+1].type == 'block':
+            try:
+                choices.remove('right')
+            except:
+                pass
+
+        return choices
+ 
+ 
+    def make_action(self,state,choices,iters):
+        if random.randint(0,500) < 200 - iters:
+            action = random.choice(choices)
+        else:
+            actionChoices = [0 for i in range(4)]
+            if 'left' in choices:
+                actionChoices[0] = 1
+            if 'up' in choices:
+                actionChoices[1] = 1
+            if 'right' in choices:
+                actionChoices[2] = 1
+            if 'down' in choices:
+                actionChoices[3] = 1
+
+            q = state.q
+            
+            for i in range(4):
+                if actionChoices[i] == 0:
+                    q[i] = np.NINF
+
+            action = self.choices[np.argmax(q)]
+
+        return action
+
+if __name__ == "__main__":
+    play(iters)
